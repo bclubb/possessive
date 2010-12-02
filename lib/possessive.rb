ecdf606cf66050146af327cb0549cd034ec2a85f
Amos Lanka <amoslanka@gmail.com>
2010-12-02 12:18:26 -0800

    added ability to customize apostrophe character

--- lib/possessive.rb
+++ lib/possessive.rb
@@ -1,5 +1,9 @@
 String.class_eval do
   def possessive
-    self + ('s' == self[-1,1] ? "'" : "'s")
+    self + ('s' == self[-1,1] ? Possessive::APOSTROPHE_CHAR : Possessive::APOSTROPHE_CHAR+"s")
   end
+end
+
+module Possessive
+  APOSTROPHE_CHAR = "’"
 end
