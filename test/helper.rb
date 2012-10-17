3a516c0aa1d7c007d6e8967a422f37b4774d3824
Aaron Patterson <aaron.patterson@gmail.com>
2012-10-17 13:57:40 -0700

    made possessives’ code 1.9 ready!!!!!

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+gem 'test-unit'
 require 'test/unit'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
--- test/test_possessive.rb
+++ test/test_possessive.rb
@@ -1,11 +1,13 @@
+# encoding: utf-8
+
 require 'helper'
 
 class TestPossessive < Test::Unit::TestCase
   test "Possessive with a normal string" do
-    assert_equal "Brian's", "Brian".possessive
+    assert_equal "Brian’s", "Brian".possessive
   end
-  
+
   test "Possessive with a string ending with s" do
-    assert_equal "Steelers'", "Steelers".possessive
+    assert_equal "Steelers’", "Steelers".possessive
   end
 end
